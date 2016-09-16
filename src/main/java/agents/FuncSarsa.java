cd5e8efea5410e55bd1507ce9290d6811be4ccbf
lsunsi <sunsi.lucas@gmail.com>
2016-09-16 18:23:23 -0300

    Agent generators (table+func)

---
+++ src/main/java/agents/FuncSarsa.java
@@ -0,0 +1,21 @@
+package main;
+
+import burlap.behavior.policy.EpsilonGreedy;
+import burlap.behavior.functionapproximation.sparse.LinearVFA;
+import burlap.behavior.functionapproximation.sparse.tilecoding.TilingArrangement;
+import burlap.behavior.functionapproximation.sparse.tilecoding.TileCodingFeatures;
+import burlap.behavior.singleagent.learning.tdmethods.vfa.GradientDescentSarsaLam;
+
+class MarioFuncSarsa {
+  final static GradientDescentSarsaLam generate(MarioDomain domain, double alpha, double gamma, double epsilon, double lambda, double weight) {
+    TileCodingFeatures tiling = new TileCodingFeatures(new MarioFeatures());
+    double[] features = new MarioFeatures().features(MarioSample.initialState);
+    for (int i=0; i<features.length; i++) features[i] = 0.5;
+    tiling.addTilingsForAllDimensionsWithWidths(features, 2, TilingArrangement.RANDOM_JITTER);
+    LinearVFA vfa = tiling.generateVFA(weight);    
+    EpsilonGreedy policy = new EpsilonGreedy(epsilon);
+    GradientDescentSarsaLam agent = new GradientDescentSarsaLam(domain, gamma, vfa, alpha, policy, 9999, lambda);
+    policy.setSolver(agent);
+    return agent;
+  }
+}
--- src/main/java/agents/TableSarsa.java
+++ src/main/java/agents/TableSarsa.java
@@ -4,15 +4,12 @@ import burlap.behavior.policy.EpsilonGreedy;
 import burlap.statehashing.simple.SimpleHashableStateFactory;
 import burlap.behavior.singleagent.learning.tdmethods.SarsaLam;
 
-class MarioTableSarsa extends SarsaLam {
-  MarioTableSarsa(MarioDomain domain, double alpha, double gamma, double epsilon, double lambda) {
-    super(
-      domain, gamma,
-      new SimpleHashableStateFactory(),
-      0.0, alpha,
-      new EpsilonGreedy(epsilon),
-      9999, lambda
-    );
-    ((EpsilonGreedy)learningPolicy).setSolver(this);
+class MarioTableSarsa {
+  final static SarsaLam generate(MarioDomain domain, double alpha, double gamma, double epsilon, double lambda) {
+    SimpleHashableStateFactory factory = new SimpleHashableStateFactory();
+    EpsilonGreedy policy = new EpsilonGreedy(epsilon);
+    SarsaLam agent = new SarsaLam(domain, gamma, factory, 0.0, alpha, policy, 9999, lambda);
+    policy.setSolver(agent);
+    return agent;
   }
 }
