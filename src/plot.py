88e084fc84fa68a92b39e696cab912cb705157f9
lsunsi <sunsi.lucas@gmail.com>
2016-09-16 08:02:18 -0300

    winrate plotting

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import statsmodels.api as sm
 import scipy.signal as sig
 import numpy as np
 
@@ -21,17 +20,27 @@ with open('logs') as f:
       except:
         pass
 
+nstates = np.array(states)
+winrates = [np.average(nstates[:i+1]) for i in range(len(nstates))]
+
 window_size = len(labels)//10 - 1
 smoothed_rewards = sig.savgol_filter(rewards, window_size, 1)
-plt.plot(labels, smoothed_rewards)
 
-win_rewards = []
-for s, r in zip(states, rewards):
-  if s == 1:
-    win_rewards.append(r)
+win_rewards = [r for (s, r) in zip(states, rewards) if s == 1]
+minwinreward = np.amin(win_rewards)
 meanwinreward = np.average(win_rewards)
-plt.plot(labels, [meanwinreward for _ in labels])
 
-print(np.average(states))
-print(meanwinreward)
+_, ax1 = plt.subplots()
+ax1.plot(labels, smoothed_rewards, 'r')
+ax1.plot(labels, [meanwinreward for _ in labels], 'k')
+
+ax2 = ax1.twinx()
+ax2.set_ylim([0, 1])
+ax2.plot(labels, winrates, 'k')
+
+print("meanstates", np.average(states))
+print("meanrewards", np.average(rewards))
+print("minwinreward", minwinreward)
+print("meanwinreward", meanwinreward)
+
 plt.show()
